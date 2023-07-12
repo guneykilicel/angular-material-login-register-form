b89ba14beff51218ce0acfde5270c25aaac4d85a
guneykilicel <kilicelguney@gmail.com>
2023-07-12 18:42:13 +0300

    login, create-user ve validasyonları

---
+++ src/app/create-user/create-user.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent {
+
+  createUserForm = new FormGroup({
+    firstName: new FormControl(null,[
+      Validators.required,
+    ]),
+    lastName: new FormControl(null,[
+      Validators.required,
+    ]),
+    email: new FormControl(null,[
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl(null,[
+      Validators.required,
+      Validators.minLength(8)
+    ]),
+    repassword: new FormControl(null,[
+      Validators.required,
+      Validators.minLength(8)
+    ]),
+    address: new FormControl(null,[
+      Validators.required,
+    ]),
+  })
+
+}
